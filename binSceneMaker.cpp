5843b7d5266fa1470083cc3079c20d2f6028f5fd
Hieu Khuong <ktgiahieu@gmail.com>
2020-11-30 16:45:46 +0700

    added STL converter

--- binSceneMaker.cpp
+++ binSceneMaker.cpp
@@ -21,6 +21,9 @@
 #include <pcl/common/geometry.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
+#include "meshSampling.h"
+#include "HPR.h"
+
 #include <bullet/btBulletDynamicsCommon.h>
 #include <bullet/LinearMath/btIDebugDraw.h>
 
@@ -98,9 +101,65 @@ int main(int argc, char* argv[])
 			//error_exit("Model load error.");
 		}
 	}
+	else if (boost::filesystem::path(setting.load_model_path).extension() == ".STL" || boost::filesystem::path(setting.load_model_path).extension() == ".stl")
+	{
+		std::string model_filename_ = setting.load_model_path.toStdString();
+		//std::cout << "Loading mesh..." << std::endl;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr model_sampling(new pcl::PointCloud<pcl::PointXYZ>());
+		model_load.reset(new pcl::PointCloud<pcl::PointXYZ>());
+		meshSampling(model_filename_, 1000000, 0.001f, false, model_sampling);
+
+		//------------------------- Calculate MEAM --------------------------------------------
+		std::vector<std::vector<float>> camera_pos(6);
+		pcl::PointXYZ minPt, maxPt, avgPt;
+
+		pcl::getMinMax3D(*model_sampling, minPt, maxPt);
+		avgPt.x = (minPt.x + maxPt.x) / 2;
+		avgPt.y = (minPt.y + maxPt.y) / 2;
+		avgPt.z = (minPt.z + maxPt.z) / 2;
+
+		float cube_length = std::max(maxPt.x - minPt.x, std::max(maxPt.y - minPt.y, maxPt.z - minPt.z));
+
+		minPt.x = avgPt.x - cube_length;
+		minPt.y = avgPt.y - cube_length;
+		minPt.z = avgPt.z - cube_length;
+		maxPt.x = avgPt.x + cube_length;
+		maxPt.y = avgPt.y + cube_length;
+		maxPt.z = avgPt.z + cube_length;
+
+		camera_pos[0] = { avgPt.x, minPt.y, avgPt.z };
+		camera_pos[1] = { maxPt.x, avgPt.y, avgPt.z };
+		camera_pos[2] = { avgPt.x, maxPt.y, avgPt.z };
+		camera_pos[3] = { minPt.x, avgPt.y, avgPt.z };
+		camera_pos[4] = { avgPt.x, avgPt.y, maxPt.z };
+		camera_pos[5] = { avgPt.x, avgPt.y, minPt.z };
+
+		//std::cout << "Preparing Multiview Model....." << std::endl;
+
+		for (int i = 0; i < static_cast<int>(camera_pos.size()); ++i)
+		{
+			//std::cout << "Preparing Viewpoint " << i << "....." << std::endl;
+
+			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz_HPR(new pcl::PointCloud<pcl::PointXYZ>());
+			HPR(model_sampling, camera_pos[i], 3, cloud_xyz_HPR);
+
+			*model_load += *cloud_xyz_HPR;
+
+		}
+
+		// centering
+		Eigen::Vector3d sum_of_pos = Eigen::Vector3d::Zero();
+		for (const auto& p : *(model_load)) sum_of_pos += p.getVector3fMap().cast<double>();
+
+		Eigen::Matrix4d transform_centering = Eigen::Matrix4d::Identity();
+		transform_centering.topRightCorner<3, 1>() = -sum_of_pos / model_load->size();
+
+		pcl::transformPointCloud(*model_load, *model_load, transform_centering);
+		pcl::transformPointCloud(*model_load, *model_load, Eigen::Vector3f(0, 0, 0), Eigen::Quaternionf(0.7071, 0, -0.7071, 0));
+	}
 	else
 	{
-		//error_exit("PLY or PCD file are only available to load.");
+		error_exit("PLY, STL or PCD file are only available to load.");
 	}
 
 	// centering
